bad7a8610ab26a343b9038d4fde8737c7f842ccf
rodvind <rod.danaei@gmail.com>
2020-03-06 15:29:34 -0700

    Add faker pkg to create random sentence and onInput method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tygame';
+  randomText = lorem.sentence();
+
+  onInput(value: string) {
+    console.log(value);
+  }
 }
